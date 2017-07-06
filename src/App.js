f45f23e3de1c777bc18a1817e06de0384485e6b9
seongyu <suengyu@me.com>
2017-07-06 17:05:02 +0900

    commit for all

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default class App extends React.Component {
     render() {
         return (
-            <div>This is the test for react applications</div>
+            <div>
+                <div>This is the test for react applications</div>
+                <div>create app now!</div>
+            </div>
         )
     }
 }
